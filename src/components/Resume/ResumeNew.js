5eaadc151aad58d87f8d72272d5d53b22788b78a
Tharun Kumar <109659829+TK-ed@users.noreply.github.com>
2023-05-23 09:41:16 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/sugi_res.pdf";
+  "https://sugavanaeshportfolio.vercel.app/static/media/sugi_res.622e49e84542e5ffe22b.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
